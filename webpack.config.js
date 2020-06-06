338f93cc62c082f6b627147e99b6ef2de5719397
Patrick Stoica <patrick.stoica@ns8.com>
2020-06-06 13:20:54 -0400

    reduce bundle size with useBuiltIns instead of adding babel polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const dev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   entry: {
-    'main': ['@babel/polyfill', './src/main'],
-    'panel': ['@babel/polyfill', './src/panel'],
+    main: './src/main',
+    panel: './src/panel'
   },
   output: {
     path: path.resolve(__dirname, 'static/dist'),
@@ -12,22 +12,35 @@ module.exports = {
   },
   devtool: dev ? 'inline-source-map' : 'source-map',
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  corejs: { version: 3 },
+                  shippedProposals: true,
+                  useBuiltIns: 'usage'
+                }
+              ]
+            ]
+          }
         }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(woff2?|ttf|eot|jpe?g|png|gif|svg)$/,
+        loader: 'file-loader'
       }
-    }, {
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader'],
-    }, {
-      test: /\.(woff2?|ttf|eot|jpe?g|png|gif|svg)$/,
-      loader: 'file-loader'
-    }]
+    ]
   },
   resolve: {
     extensions: ['.js']
